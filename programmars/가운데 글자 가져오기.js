f5e5788cf5c4a016b7dd3dc28cd0bc753706d18b
waterfall <cvb575859@gmail.com>
2024-01-03 14:10:39 +0900

    Create 가운데 글자 가져오기.js

---
+++ programmars/가운데 글자 가져오기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    let answer = '';
+    if (s.length % 2 === 1) {
+        answer = s[parseInt(s.length / 2)];
+    } else {
+        answer = s[parseInt(s.length / 2) - 1] + s[parseInt(s.length / 2)];
+    }
+    return answer;
+}
+
+console.log(solution('qwer'));
