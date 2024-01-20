6e3f17803f2e7a159a76893392d54488a43ab633
waterfall <cvb575859@gmail.com>
2024-01-20 16:03:01 +0900

    Create 1431. Kids With the Greatest Number of Candies.js

---
+++ leetcode/1431. Kids With the Greatest Number of Candies.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+const kidsWithCandies = function (candies, extraCandies) {
+    const result = candies.map((candy) => {
+        return candy + extraCandies >= Math.max(...candies);
+    });
+
+    return result;
+};
+
+console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
