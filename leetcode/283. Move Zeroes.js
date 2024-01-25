0b268a68174f8b48f458772aba28e00745b93a15
waterfall <cvb575859@gmail.com>
2024-01-25 23:02:57 +0900

    Create 283. Move Zeroes.js

---
+++ leetcode/283. Move Zeroes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = function (nums) {
+    let zeroCount = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            zeroCount++;
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+
+    nums.push(...new Array(zeroCount).fill(0));
+
+    return nums;
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
