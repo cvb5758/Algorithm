f85c2b4f6116a8c7d34a731b92fc3515066c2f92
waterfall <cvb575859@gmail.com>
2024-01-26 18:45:34 +0900

    Create 1679. Max Number of K-Sum Pairs.js

---
+++ leetcode/1679. Max Number of K-Sum Pairs.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const maxOperations = function (nums, k) {
+    nums.sort((a, b) => a - b);
+    let left = 0;
+    let right = nums.length - 1;
+    let count = 0;
+
+    while (left < right) {
+        const sum = nums[left] + nums[right];
+        if (sum === k) {
+            count++;
+            left++;
+            right--;
+        } else if (sum < k) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    console.log(nums);
+    return count;
+};
+
+console.log(maxOperations([1, 2, 3, 4], 5));
