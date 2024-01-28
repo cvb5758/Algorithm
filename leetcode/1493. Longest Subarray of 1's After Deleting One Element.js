bdb2ddd0a75be9d01253f129ed936c7180f0b787
waterfall <cvb575859@gmail.com>
2024-01-28 23:10:42 +0900

    Create 1493. Longest Subarray of 1's After Deleting One Element.js

---
+++ leetcode/1493. Longest Subarray of 1's After Deleting One Element.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestSubarray = function (nums) {
+  let left = 0;
+  let max = 0;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      count++;
+    }
+    while (count > 1) {
+      if (nums[left] === 0) {
+        count--;
+      }
+      left++;
+    }
+    max = Math.max(max, i - left);
+  }
+  return max;
+};
+
+console.log(longestSubarray([1, 1, 1]));
