c25b967f943a2485aeb32da88d91820710d707d4
waterfall <cvb575859@gmail.com>
2024-02-02 23:38:22 +0900

    Update 2890. 다른 풀이 방법

--- leetcode/2390. Removing Stars From a String.js
+++ leetcode/2390. Removing Stars From a String.js
@@ -1,19 +1,35 @@
-/**
- * @param {string} s
- * @return {string}
- */
+// /**
+//  * @param {string} s
+//  * @return {string}
+//  */
+// const removeStars = function (s) {
+//   let result = '';
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === '*') {
+//       result = result.slice(0, -1);
+//     } else {
+//       result += s[i];
+//     }
+//   }
+
+//   return result;
+// };
+
+// console.log(removeStars('leet**cod*e'));
+
 const removeStars = function (s) {
-  let result = '';
+  const stack = [];
 
   for (let i = 0; i < s.length; i++) {
     if (s[i] === '*') {
-      result = result.slice(0, -1);
+      stack.pop();
     } else {
-      result += s[i];
+      stack.push(s[i]);
     }
   }
 
-  return result;
+  return stack.join('');
 };
 
 console.log(removeStars('leet**cod*e'));
