60bff825d6c619af5333a7822e1e54fffb2daacd
waterfall <cvb575859@gmail.com>
2024-05-12 19:36:21 +0900

    Create 할인 행사.js

---
+++ programmars/할인 행사.js
@@ -0,0 +1,17 @@
+function solution(want, number, discount) {
+  let answer = 0;
+
+  for (let i = 0; i < discount.length - 9; i++) {
+    let slice = discount.slice(i, i + 10);
+
+    let flag = true;
+
+    for (let j = 0; j < want.length; j++) {
+      if (slice.filter((item) => item === want[j]).length !== number[j]) {
+        flag = false;
+      }
+    }
+    if (flag) answer++;
+  }
+  return answer;
+}
